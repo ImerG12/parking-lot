6c027ab7b4b39eff4ec3f8f2573eeb274d058d84
ImerG12 <77695762+ImerG12@users.noreply.github.com>
2021-12-06 12:37:46 -0800

    Add files via upload

---
+++ car.py
@@ -0,0 +1,63 @@
+class Car:
+    def __init__(self,licenseNumber):
+        self.licenseNumber = licenseNumber
+
+class ParkingSlot:
+    def __init__(self, row, spotNumber, level, car):
+        self.row = row
+        self.spotNumber = spotNumber
+        self.level = level
+        self.car = car
+
+    def isAvailable(self):
+        return self.car == None
+
+    def park(self, vehicle):
+        self.car = vehicle
+
+    def removeVehicle(self):
+        self.car
+
+class Level:
+    def __init__(self, rows, levelNumber):
+        self. levelNumber = levelNumber
+        self.rows = rows
+        self.spotsPerRow = 2
+        self.parkingSlots = []
+        self.availableSpots = rows * self.spotsPerRow
+
+    def findAvailableSlot(self):
+        if(self.availableSpots <=0):
+            return None
+        else:
+            if(len(self.parkingSlots) == 0) :
+                return ParkingSlot(0,0,0,None)
+            else:
+                lastCarParked = self.parkingSlots[-1]
+            if(lastCarParked.spotNumber<self.spotsPerRow):
+                return ParkingSlot(lastCarParked.row, lastCarParked. spotNumber+1, self. levelNumber, None)
+            if(lastCarParked. row<self. rows) :
+                return ParkingSlot(lastCarParked.row+1,0,self.leveNumber,None)
+
+    def park(self,vehicle):
+        freeParkingSpot = self.findAvailableSlot()
+        if(not(freeParkingSpot)):
+            return False
+
+        else:
+            freeParkingSpot.park(vehicle)
+            self.parkingSlots.append(freeParkingSpot)
+            self.avaiableSpots-=1
+            return True
+
+class ParkingLot:
+    def __init__(self, levels):
+        self.levels = levels
+
+    def park(self, car):
+        for level in self.levels:
+            if(level.park(car)):
+                return True
+            else:
+                continue;
+        return False
